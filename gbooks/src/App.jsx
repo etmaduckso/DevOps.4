d277211876d417e12e86f1246f4935f56f909fa2
LAKE <etmaduckso@hotmail.com>
2021-05-28 06:57:51 -0300

    gbooks.2
    final CSS para teste JSX xonstrução

--- gbooks/src/App.jsx
+++ gbooks/src/App.jsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import {
+  InputGroup,
+  Input,
+  InputGroupAddon,
+  Button,
+  FormGroup,
+  Label,
+  Spinner
+} 
+from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import axios from 'axios';
+import BookCard from './BookCard.jsx';
 
 function App() {
-  return (
-    <div >
-        start
-    </div>
-  );
-}
-
-export default App;
+  // States
+  const [maxResults, setMaxResults] = useState(10);
+  const [startIndex, setStartIndex] = useState(1);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [cards, setCards] = useState([]);
+  // Handle Search
+  const handleSubmit = () => {
+    setLoading(true);
+    if (maxResults > 40 || maxResults < 1) {
+      toast.error('max results must be between 1 and 40');
+    } else {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
+        )
+        .then(res => {
+          if (startIndex >= res.data.totalItems || startIndex < 1) {
+            toast.error(
+              `max reults must be between 1 and ${res.data.totalItems}`
+            );
+          } else {
+            if (res.data.items.length > 0) {
+              setCards(res.data.items);
+              setLoading(false);
+            }
+          }
+        })
+        .catch(err => {
+          setLoading(true);
+          console.log(err.response);
+        });
+    }
+  };
+  // Main Show Case
+  const mainHeader = () => {
