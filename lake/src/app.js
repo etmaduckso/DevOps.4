73572c0d2c21ee2ba5bc2080c5d991f2076afb90
LAKE <etmaduckso@hotmail.com>
2021-06-02 00:53:28 -0300

    ROTAS testando

--- lake/src/app.js
+++ lake/src/app.js
@@ -1,21 +1,25 @@
+'use strict'
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-  const route = router.get
-('/', (req, res, next) => 
-{
-        res.status(200).send
-        ({
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const route = router.get('/', (req, res, next) => {
+        res.status(200).send({
             title: "SERVER LAKE - API", 
             version: "0.0.2"
         });
 });
-/* const create = router.post('/', (req, res, next) =>
-{
+
+const create = router.post('/', (resq, res, next) => {
       res.status(201).send(req.body);
-});*/
+      });
 app.use('/', route);
-//app.use('/product');
+app.use('/products, create');
+
 module.exports = app;
